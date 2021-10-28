11c0c4261c10793f48cd1d76e229630d1ce60f3c
Laarup <laarup85@gmail.com>
2021-10-28 20:09:49 +0300

    different screens

---
+++ screens/HomeScreen.js
@@ -0,0 +1,85 @@
+import React, { useState } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Cancelbutton from '../components/CancelButton';
+import StartButtons from '../components/StartButtons';
+import Timer from '../components/Timer';
+import dim from '../lib/dimmer';
+import { activateKeepAwake, deactivateKeepAwake } from 'expo-keep-awake';
+
+const HomeScreen = () => {
+    const [start, setStart] = useState(false);
+    const [time, setTime] = useState(false);
+
+    const startTimer = (time) => {
+        if (start) {
+            setStart(false);
+            setTime(0);
+        }
+        dim(0.1);
+        setStart(true);
+        setTime(time);
+        activateKeepAwake();
+    }
+
+    const stopTimer = () => {
+        dim(1);
+        setStart(false);
+        deactivateKeepAwake();
+    }
+
+    return (
+        <View style={styles.container}>
+            <View style={styles.titleContainer}>
+                <Text style={styles.title}>Meditatsiooni kestus:</Text>
+            </View>
+            <View style={styles.startButtonsContainer}>
+                <StartButtons startTimer={startTimer} />
+            </View>
+            <View style={styles.timerContainer}>
+                <Timer start={start} time={time} stopTimer={stopTimer} />
+            </View>
+            <View style={styles.cancelButton}>
+                <Cancelbutton stopTimer={stopTimer} />
+            </View>
+        </View>
+    );
+}
+
+export default HomeScreen;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#5b6980',
+        alignItems: 'center',
+        paddingTop: 130,
+        flexDirection: 'column',
+    },
+    titleContainer: {
+        flex: 1
+    },
+    title: {
+        color: '#ccc',
+        fontWeight: 'bold',
+        fontSize: 30,
+    },
+    startButtonsContainer: {
+        flex: 2,
+        margin: 'auto'
+    },
+    buttonText: {
+        color: '#ccc',
+        fontSize: 30,
+        fontWeight: 'bold'
+    },
+    timerContainer: {
+        flex: 3
+    },
+    countdownTime: {
+        fontSize: 60,
+        color: '#ccc',
+    },
+    cancelButton: {
+        flex: 2,
+    },
+});
